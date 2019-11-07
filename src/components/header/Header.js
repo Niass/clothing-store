bf28c936df20755ae791f144022935a86d3c2094
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-11-08 00:02:14 +0100

    Created a card-icon component and display it in header componenent

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,6 +7,7 @@ import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './Header.scss';
+import CartIcon from '../cart-icon/CartIcon';
 
 const Header = ( {currentUser} ) => (
   <div className='header'>
@@ -30,6 +31,7 @@ const Header = ( {currentUser} ) => (
           SIGN IN
         </Link>
       }
+      <CartIcon/>
     </div>
   </div>
 )
