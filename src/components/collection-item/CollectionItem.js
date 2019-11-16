c8e55ee88a6cbfeb776b4372ee3b19ccded28061
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-11-16 11:40:12 +0100

    Created cart-item component and connect our cart dropdown with cartItem reducer

--- src/components/collection-item/CollectionItem.js
+++ src/components/collection-item/CollectionItem.js
@@ -1,27 +1,30 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import "./CollectionItem.scss";
-import CustomButton from "../custom-button/CustomButton";
-import { addItem } from "../../redux/cart/cart-actions";
+import './CollectionItem.scss';
+import CustomButton from '../custom-button/CustomButton';
+import { addItem } from '../../redux/cart/cart-actions';
 
-const CollectionItem = ({item, addItem}) => {
-  const {name, price, imageUrl } = item;
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
   return (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name"> {name} </span>
-      <span className="price"> {price} </span>
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name"> {name} </span>
+        <span className="price"> {price} </span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
     </div>
-    <CustomButton onClick={()=> addItem(item)} inverted>Add to cart</CustomButton>
-  </div>
-)};
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item))
