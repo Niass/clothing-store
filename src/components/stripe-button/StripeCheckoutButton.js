9a79995ee54df5b12f6dc594a682b8e0a7f7b812
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-11-27 11:25:20 +0100

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/StripeCheckoutButton.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_live_y9OiLJeWCdLrfFeTvSLkbMvn';
+  const onToken = token => {
+    console.log(token);
+
+    alert('Payment successful');
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image=""
+      description={`Your total is ${price}€`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/CheckoutPage.js
+++ src/pages/checkout/CheckoutPage.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import StripeCheckoutButton from '../../components/stripe-button/StripeCheckoutButton';
 
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+import {
+  selectCartItems,
+  selectCartTotal
+} from '../../redux/cart/cart.selectors';
 
 import './CheckoutPage.scss';
 import CheckoutItem from '../../components/checkout-item/CheckoutItem';
 
-const CheckoutPage = ({cartItems, total}) => (
+const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
     <div className="checkout-header">
       <div className="header-block">
@@ -26,20 +30,24 @@ const CheckoutPage = ({cartItems, total}) => (
         <span>Remove</span>
       </div>
     </div>
-    {
-      cartItems.map(cartItem =>
-       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-      )
-    }
+    {cartItems.map(cartItem => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
     <div className="total">
       <span>TOTAL: €{total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br/>
+      4242 4242 4242 4242 - EXP 01/20 - CVV:123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
   total: selectCartTotal
-})
+});
 
 export default connect(mapStateToProps)(CheckoutPage);
