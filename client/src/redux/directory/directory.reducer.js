26c0f73a2db8b07ae9d32211bec283c98e5a5620
Ibrahima Niass <cheroubawan@hotmail.fr>
2020-01-25 20:06:36 +0100

    updated image links

--- client/src/redux/directory/directory.reducer.js
+++ client/src/redux/directory/directory.reducer.js
@@ -2,32 +2,37 @@ const INITIAL_STATE = {
   sections: [
     {
       title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      imageUrl:
+        'https://images.unsplash.com/photo-1530062329328-9734c43ae31b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
       id: 1,
       linkUrl: 'shop/hats'
     },
     {
       title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      imageUrl:
+        'https://images.unsplash.com/photo-1521235398451-3c8e83566923?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
       id: 2,
       linkUrl: 'shop/jackets'
     },
     {
       title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      imageUrl:
+        'https://images.unsplash.com/photo-1506544777-64cfbe1142df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
       id: 3,
       linkUrl: 'shop/sneakers'
     },
     {
       title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      imageUrl:
+        'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
       size: 'large',
       id: 4,
       linkUrl: 'shop/womens'
     },
     {
       title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl:
+        'https://images.unsplash.com/photo-1480455624313-e29b44bbfde1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens'
@@ -35,12 +40,11 @@ const INITIAL_STATE = {
   ]
 };
 
-
-const directoryReducer= (state= INITIAL_STATE, action)=> {
-  switch(action.type) {
+const directoryReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
     default:
       return state;
   }
-}
+};
 
 export default directoryReducer;
